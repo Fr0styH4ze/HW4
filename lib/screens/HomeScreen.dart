5fc071316d47e97d3eff21c7bd49a24955912a87
Fr0styH4ze <ali1420alkorbi@gmail.com>
2026-01-22 16:34:30 +0300

    Added appbar, body, container and text

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -6,10 +6,27 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Homework 4"),),
-      body: Column(
-        children: [
-        ],
+      appBar: AppBar(
+        title: const Text("Homework 4"),
+        backgroundColor: Colors.lightBlue[100],
+      ),
+      body: Center(
+        child: Row(
+          children: [
+            Container(
+              height: 100,
+              width: 200,
+              color: Colors.blue,
+              child: Text("Container1"),
+            ),
+            Container(
+              height: 100,
+              width: 200,
+              color: Colors.red,
+              child: Text("Container2"),
+            ),
+          ],
+        ),
       ),
     );
   }
